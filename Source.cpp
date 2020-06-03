a29f127c7b4dfbe086ed5cd214bcbd9c61ec81ac
Artem Arhipov <artemiy3893@gmail.com>
2020-06-03 18:31:07 +0300

    second carpter

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,29 @@
 #include<iostream>
 #include<iomanip>
 
-#define SIZE 10
+#define SIZE 5
 
 using namespace std;
 
+int currentNumb = 1;
+int max_pos_x = 2, max_pos_y = 3;
+int now_pos_x = SIZE/2, now_pos_y = SIZE/2 - 1;
+
+
+bool out() {
+	if (currentNumb == SIZE * SIZE )
+		return true;
+	return false;
+}
+
+
 int main() {
 	int arr[SIZE][SIZE];
-	int currentNumb = 1;
-
-	for ( int i=0; i<SIZE; i++)
-		for (int j = 0; j < SIZE; j++) {
-			if (i % 2 == 0)
-				arr[i][j] = currentNumb;
-			else
-				arr[i][SIZE - j - 1] = currentNumb;
-
-			currentNumb += 1;
-		}
-
+	
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < SIZE; j++)
+			arr[i][j] = i + j;
+	
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++)
 			cout <<setw(3)<<arr[i][j] << " ";
