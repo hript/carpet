782a5e809d2396d4e697da79a2ffe4ab08b4ac81
Artem Arhipov <artemiy3893@gmail.com>
2020-06-03 18:41:21 +0300

    Snezhinka

--- Source.cpp
+++ Source.cpp
@@ -5,24 +5,16 @@
 
 using namespace std;
 
-int currentNumb = 1;
-int max_pos_x = 2, max_pos_y = 3;
-int now_pos_x = SIZE/2, now_pos_y = SIZE/2 - 1;
-
-
-bool out() {
-	if (currentNumb == SIZE * SIZE )
-		return true;
-	return false;
-}
-
-
 int main() {
-	int arr[SIZE][SIZE];
-	
-	for (int i = 0; i < SIZE; i++)
-		for (int j = 0; j < SIZE; j++)
-			arr[i][j] = i + j;
+	int arr[SIZE][SIZE] = { 0 };
+	system("color F0");
+	for ( int i=0; i<SIZE; i++)
+		for (int j = 0; j < SIZE; j++) {
+			if (i == j || (i == SIZE - j - 1))
+				arr[i][j] = 1; 
+			if (i == SIZE / 2 || j == SIZE / 2)
+				arr[i][j] = 1; 
+		}
 	
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++)
